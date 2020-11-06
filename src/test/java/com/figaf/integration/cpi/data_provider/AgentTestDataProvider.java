fcda10ce5a140046f39c97e6450c4b241161016d
Arsenii Istlentev <ais@figaf.com>
2020-11-06 15:03:35 +0300

    IRT-1891 Test parallel authorization

--- src/test/java/com/figaf/integration/cpi/data_provider/AgentTestDataProvider.java
+++ src/test/java/com/figaf/integration/cpi/data_provider/AgentTestDataProvider.java
@@ -1,6 +1,7 @@
 package com.figaf.integration.cpi.data_provider;
 
 import com.figaf.integration.common.data_provider.AbstractAgentTestDataProvider;
+import com.figaf.integration.common.data_provider.AgentTestData;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 
@@ -23,4 +24,17 @@ public class AgentTestDataProvider extends AbstractAgentTestDataProvider {
                 ))
         );
     }
+
+    public static AgentTestData buildAgentTestDataForCf1() {
+        return buildAgentTestData(Paths.get("src/test/resources/agent-test-data/cpi-cf-1"));
+    }
+
+    public static AgentTestData buildAgentTestDataForCf2() {
+        return buildAgentTestData(Paths.get("src/test/resources/agent-test-data/cpi-cf-2"));
+    }
+
+    public static AgentTestData buildAgentTestDataForNeo() {
+        return buildAgentTestData(Paths.get("src/test/resources/agent-test-data/cpi-neo-1"));
+    }
+
 }
