047b394e91f467299ff7dab19f510d4e33b7e5c8
Fledi Ymerasi <fly@figaf.com>
2022-06-21 11:44:42 +0200

    IRT-3094: Allow inclusion of custom headers on message processing logs

--- src/main/java/com/figaf/integration/cpi/utils/CpiApiUtils.java
+++ src/main/java/com/figaf/integration/cpi/utils/CpiApiUtils.java
@@ -1,9 +1,14 @@
 package com.figaf.integration.cpi.utils;
 
+import com.figaf.integration.cpi.entity.message_processing.CustomHeaderProperty;
 import org.apache.commons.lang3.time.FastDateFormat;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Arsenii Istlentev
@@ -29,5 +34,26 @@ public class CpiApiUtils {
         } catch (Exception ex) {
             throw new RuntimeException("Can't parse date: ", ex);
         }
+    }
+
+    public static List<CustomHeaderProperty> parseCustomerHeaderProperties(JSONObject messageProcessingLogElement) {
+        List<CustomHeaderProperty> customHeaderProperties = new ArrayList<>();
+        if (messageProcessingLogElement.isNull("CustomHeaderProperties")) {
+            return customHeaderProperties;
+        }
+        JSONObject jsonObject = messageProcessingLogElement.getJSONObject("CustomHeaderProperties");
+        if (jsonObject.isNull("results")) {
+            return customHeaderProperties;
+        }
+        JSONArray results = jsonObject.getJSONArray("results");
+        for (int i = 0; i < results.length(); i++) {
+            JSONObject result = results.getJSONObject(i);
+            String id = (String) result.get("Id");
+            String name = (String) result.get("Name");
+            String value = (String) result.get("Value");
+            CustomHeaderProperty customHeaderProperty = new CustomHeaderProperty(id, name, value);
+            customHeaderProperties.add(customHeaderProperty);
+        }
+        return customHeaderProperties;
     }
 }
