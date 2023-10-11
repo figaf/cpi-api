37b6827475571cec84103988c62652e576e96b9a
Ilya Nesterov <iln@figaf.com>
2023-10-11 16:13:51 +0300

    IRT-4038 added comment

--- src/main/java/com/figaf/integration/cpi/client/ConfigurationsClient.java
+++ src/main/java/com/figaf/integration/cpi/client/ConfigurationsClient.java
@@ -31,6 +31,15 @@ public class ConfigurationsClient extends CpiBaseClient {
 
         if (requestContext.isIntegrationSuite()) {
             path = API_CONFIGURATIONS;
+            /*
+            It's required to update restTemplateWrapperKey because requests to the main web api host and IS host don't have compatible session.
+            In other words, when they are executed 1 by 1, for the second request it requires to process authentication flow fully
+            e2e to get it successfully processed.
+            But it won't work in that way because of the fix for "waiting" authentication attempts that skips authentication
+            for all threads except the first. See comments in BaseClient.makeAuthRequestsWithLock.
+
+            It's more efficient to keep a different session context for requests to IS host
+             */
             requestContext.setRestTemplateWrapperKey(requestContext.getRestTemplateWrapperKey() + IS_URL_KEY_SUFFIX);
             requestContext.getConnectionProperties().setHost(requestContext.getIntegrationSuiteUrl());
             extractParamsFunction = ConfigurationsParser::buildCpiIsConfigurations;
