fe494a8b0176e3954dae6e69567d2a0dc3b93d32
kosta <koc@figaf.com>
2026-01-20 17:19:21 +0200

    IRT-5510 Add support of deploy/undeploy from Landscape overview for other CPI artifact types -release

--- src/main/java/com/figaf/integration/cpi/client/IntegrationContentWebApiClient.java
+++ src/main/java/com/figaf/integration/cpi/client/IntegrationContentWebApiClient.java
@@ -37,7 +37,7 @@ public class IntegrationContentWebApiClient extends IntegrationContentAbstractCl
 
     private static final String INTEGRATION_COMPONENTS_LIST_API = "/Operations/com.sap.it.op.tmn.commands.dashboard.webui.IntegrationComponentsListCommand";
     private static final String INTEGRATION_COMPONENT_DETAIL_API = "/Operations/com.sap.it.op.tmn.commands.dashboard.webui.IntegrationComponentDetailCommand?artifactId=%s";
-    private static final String DELETE_CONTENT_API = "%s/Operations/com.sap.it.nm.commands.deploy.DeleteContentCommand";
+    private static final String DELETE_CONTENT_API = "/Operations/com.sap.it.nm.commands.deploy.DeleteContentCommand";
 
     private static final Map<String, String> NODE_TYPE_MAPPING = new HashMap<>();
 
